30a233b33c360c8f0a0da9392a9628cc53d068ac
vitor4818 <vitorg.martins4818@gmail.com>
2024-05-26 19:25:11 -0300

    WIP Função de adicionar tarefa completa

--- js/script.js
+++ js/script.js
@@ -5,15 +5,16 @@ let listItems = []
 
 
 function addNovaTarefa(event){
-
     event.preventDefault() //Evitar da página ser atualizada ao clicar no botão
+
+    if(newTask.value ==''){//Caso aperte o botão sem digitar nada
+        window.alert("Digite algo primeiro!")
+        return;
+    }
     listItems.push(newTask.value) //Adiciona o item no array
     console.log(listItems)//Exibe na tela uai
     newTask.value = ''; // Limpa os campos
-
-
 }
-
 
 buttonAddTarefa.addEventListener('click', addNovaTarefa)
 
